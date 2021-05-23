679f43d941aa413657cb5519faa2854eb8a64c0e
Gaurav Sood <soodg@amazon.com>
2021-05-23 21:53:09 +0530

    IAmBot

---
+++ DiscordBot.py
@@ -0,0 +1,59 @@
+import os
+import random
+
+import discord
+
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(
+        f'{client.user} has connected to Discord!\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, Welcome to my Discord Server'
+    )
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+client.run(TOKEN)
---
+++ README.md
@@ -0,0 +1,8 @@
+# IAmBot
+
+## Discord Bot
+
+### prerequisites:
+- create a .env file with following variables
+  - DISCORD_BOT_TOKEN = {your_bot_token}
+  - DISCORD_GUILD = {your_guild_name}
